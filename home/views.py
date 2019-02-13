490dcef548c91b83de8c59c9e9677ac2faa617fa
kang yewon <kyeah01@gmail.com>
2019-02-13 02:28:25 +0000

    03_template variable

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from pprint import pprint
+import random
 
 # Create your views here.
 def index(request):
@@ -7,3 +8,9 @@ def index(request):
     # print(type(request))
     # pprint(request.META)
     return HttpResponse('Welcome to Django!')
+    
+def dinner(request):
+    menu = ['슈프림치킨', '철판볶음밥', '연어', '소고기', '장조림']
+    pick = random.choice(menu)
+    # return HttpResponse(random.choice(menu))
+    return render(request, 'dinner.html', {'menus' : menu, 'pick' : pick})
