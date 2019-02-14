0118c9ad28501aead018b24963bc146f42b54d14
kang yewon <kyeah01@gmail.com>
2019-02-14 03:16:58 +0000

    10_Template name spacing

--- django_intro/urls.py
+++ django_intro/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('home/', include('home.urls')),
+    path('utilities/', include('utilities.urls')),
     path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
 ]
--- home/views.py
+++ home/views.py
@@ -9,36 +9,36 @@ def index(request):
     # print(type(request))
     # pprint(request.META)
     # return HttpResponse('Welcome to Django!')
-    return render(request, 'index.html')
+    return render(request, 'home/index.html')
     
 def dinner(request):
     menu = ['슈프림치킨', '철판볶음밥', '연어', '소고기', '장조림']
     pick = random.choice(menu)
     # return HttpResponse(random.choice(menu))
-    return render(request, 'dinner.html', {'menus' : menu, 'pick' : pick})
+    return render(request, 'home/dinner.html', {'menus' : menu, 'pick' : pick})
     
 def hello(request, name):
-    return render(request, 'hello.html', {'name': name })
+    return render(request, 'home/hello.html', {'name': name })
     
 def cube(request, num):
     cube = num **3
-    return render(request, 'cube.html', {'num': num, 'cube':cube })
+    return render(request, 'home/cube.html', {'num': num, 'cube':cube })
     
 def ping(request):
-    return render(request, 'ping.html')
+    return render(request, 'home/ping.html')
     
 def pong(request):
     print(request.GET)
     data = request.GET.get('data')
-    return render(request, 'pong.html', {'data':data })
+    return render(request, 'home/pong.html', {'data':data })
 
 def user_new(request):
-    return render(request, 'user_new.html')
+    return render(request, 'home/user_new.html')
 
 def user_create(request):
     nickname = request.POST.get('nickname')
     password = request.POST.get('password')
-    return render(request, 'user_create.html', {'nickname':nickname, 'password':password})
+    return render(request, 'home/user_create.html', {'nickname':nickname, 'password':password})
 
 
 
@@ -52,7 +52,7 @@ def template_example(request):
     messages = ['apple', 'banana', 'cherry', 'mango']
     empty_list = []
     datetimenow = datetime.now()
-    return render(request, 'template_example.html',
+    return render(request, 'home/template_example.html',
                     {'my_list':my_list,
                     'my_sentence':my_sentence,
                     'messages': messages,
@@ -61,4 +61,4 @@ def template_example(request):
                     })
 
 def static_example(request):
-    return render(request, 'static_example.html')
+    return render(request, 'home/static_example.html')
